3c7712bcbb3c5b5f31ce943ddb6ceb72db78cc90
CYPARK <chanyoung990704@naver.com>
2023-10-05 20:27:52 +0900

    move grid에 자동완성 기능 추가

--- src/api/PostApiService.jsx
+++ src/api/PostApiService.jsx
@@ -3,3 +3,4 @@ import { appClient } from "../pages/ApiClient";
 
 export const register = (registerForm) => appClient.post('/api/register', registerForm)
 export const likes = (category, id) => appClient.post(`/api/${category}/${id}`)
+export const movieSearch = (keyword) => appClient.get(`/autocomplete/search?prefix=${keyword}`)
